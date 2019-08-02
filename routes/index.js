5be2af3632117800a39fe218804084beb4a23b82
sakkuntyo <d-sasaki@know-net.co.jp>
2019-08-02 18:38:40 +0900

    slackapiのリクエストURL確認に応答する様に変更

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/slack', function(req, res, next) {
+  console.log("フックされました")
+  res.send(req.body.challenge)
 });
 
 module.exports = router;
