ecb27c71fe2fb87e3e3ccd0c6d739fdb35dad777
sakkuntyo <d-sasaki@know-net.co.jp>
2019-08-03 02:22:08 +0900

    オウム返し実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var { WebClient } = require('@slack/client');
+var token = "<Bot User OAuth Access Token>";
+var web = new WebClient(token);
+
+
+// Request URL challenge用
+/*
 router.post('/slack', function(req, res, next) {
   console.log("フックされました")
+  console.log(req)
   res.send(req.body.challenge)
 });
+//*/
+
+// オウム返し用
+///*
+router.post('/slack', function(req, res, next) {
+  // botの発言でないなら処理
+  if (!req.body.event.bot_id) {
+    console.log(req.body.event)
+    web.chat.postMessage({
+      as_user: true,
+      channel: req.body.event.channel,
+      text: req.body.event.text
+    })
+  }
+  next;
+});
+//*/
 
 module.exports = router;
