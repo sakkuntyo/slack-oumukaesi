7fcdc54df6927abf681490fb7bf7046554d35da4
sakkuntyo <d-sasaki@know-net.co.jp>
2019-08-03 15:33:32 +0900

    疎通確認用のAPIとオウム返し用のAPIを統一

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,12 @@ var { WebClient } = require('@slack/client');
 var token = "<Bot User OAuth Access Token>";
 var web = new WebClient(token);
 
-
-// Request URL challenge用
-/*
 router.post('/slack', function(req, res, next) {
-  console.log("フックされました")
-  console.log(req)
+  // 疎通確認用
   res.send(req.body.challenge)
-});
-//*/
-
-// オウム返し用
-///*
-router.post('/slack', function(req, res, next) {
   // botの発言でないなら処理
   if (!req.body.event.bot_id) {
+    console.log("a")
     console.log(req.body.event)
     web.chat.postMessage({
       as_user: true,
